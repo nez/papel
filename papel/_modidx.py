6d424881f1d569ba6d6ad20ae07d63e81a77e2f0
Gu Andres <andres@robotani.ca>
2023-04-26 14:45:42 -0600

    save

--- papel/_modidx.py
+++ papel/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://nez.github.io',
                 'git_url': 'https://github.com/nez/papel',
                 'lib_path': 'papel'},
-  'syms': {'papel.core': {'papel.core.dl': ('core.html#dl', 'papel/core.py')}}}
+  'syms': { 'papel.core': { 'papel.core.dataset': ('core.html#dataset', 'papel/core.py'),
+                            'papel.core.dl': ('core.html#dl', 'papel/core.py'),
+                            'papel.core.papers_vector': ('core.html#papers_vector', 'papel/core.py'),
+                            'papel.core.pdf': ('core.html#pdf', 'papel/core.py'),
+                            'papel.core.results': ('core.html#results', 'papel/core.py'),
+                            'papel.core.train_test_partition': ('core.html#train_test_partition', 'papel/core.py')}}}
